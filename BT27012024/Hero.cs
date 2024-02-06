48996c07bff6efdbae0bcf299faba949f71d08bf
DESKTOP-1OGKUDR\Long <nguyenhoanglong112001@gmail.com>
2024-02-06 09:05:36 +0700

    no message

---
+++ BT27012024/Hero.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT27012024
+{
+    public enum HeroType
+    {
+        Agility,
+        Strength,
+        Intelligence,
+        Universal
+    }
+    public class Hero
+    {
+        public string heroname;
+        public HeroType herotype;
+        public double BaseDame { get; protected set; } = 50;
+        public int BaseHealth { get; protected set; } = 300;
+        public int BaseMana { get; protected set; } = 200;
+        public double BaseSpeed { get; protected set; } = 10;
+        public double BaseAmor { get; protected set; } = 3;
+        public int agility { get; protected set; }
+        public int strength { get; protected set; }
+        public int intelligence { get; protected set; }
+
+        public Hero() { }
+
+        public Hero(string heroname)
+        {
+            this.heroname = heroname;
+            herotype = (HeroType)GameHelper.GetRandomValue(0, 4);
+            agility = GameHelper.GetRandomValue(0, 81);
+            strength = GameHelper.GetRandomValue(0, 81);
+            intelligence = GameHelper.GetRandomValue(0, 81);
+        }
+
+        public void heroatribute()
+        {
+            if (herotype == HeroType.Agility)
+            {
+                BaseDame += agility;
+                BaseAmor += 0.14 * agility;
+                BaseSpeed += agility;
+            }
+            if (herotype == HeroType.Strength)
+            {
+                BaseHealth += 19 * strength;
+                BaseDame += strength;
+            }
+            if (herotype == HeroType.Intelligence)
+            {
+                BaseDame += intelligence;
+                BaseMana += intelligence;
+            }
+            if (herotype == HeroType.Universal)
+            {
+                BaseDame += 0.4 * (strength + agility + intelligence);
+            }
+        }
+    }
+}
--- BT27012024/Program.cs
+++ BT27012024/Program.cs
@@ -11,7 +11,8 @@ namespace BT27012024
     internal class Program
     {
         public static Item[] items = null;
-
+        public static Dictionary<ItemType, List<Item>> Itemclassify = new Dictionary<ItemType, List<Item>>();
+        public static List<Item> itemlist = new List<Item>();
         static void Main(string[] args)
         {
             items = new Item[10];
@@ -27,6 +28,9 @@ namespace BT27012024
                         ShowAllItem();
                         break;
                     case 3:
+                        ClassifyItem();
+                        break;
+                    case 4:
                         Environment.Exit(0);
                         break;
                 }
@@ -40,7 +44,8 @@ namespace BT27012024
             Console.WriteLine("Your gold: " + CurrencyManager.currentGold);
             Console.WriteLine("1. Create Item");
             Console.WriteLine("2. Show All Item");
-            Console.WriteLine("3. Exit");
+            Console.WriteLine("3. Clasify Item");
+            Console.WriteLine("4. Exit");
             Console.WriteLine("chose function: ");
             int key = int.Parse(Console.ReadLine());
 
@@ -56,6 +61,12 @@ namespace BT27012024
                 ItemType type = (ItemType)GameHelper.GetRandomValue(0, 4);
                 Item newitem = inven.CreateITem($"{type}", type);
                 items[i] = newitem;
+                itemlist.Add(items[i]);
+                if (!Itemclassify.ContainsKey(type))
+                {
+                    Itemclassify[type] = new List<Item>();
+                }
+                Itemclassify[type].Add(newitem);
                 Console.WriteLine($"{i + 1} . {items[i].itemname}");
                 Thread.Sleep(100);
             }
@@ -106,6 +117,64 @@ namespace BT27012024
                     items[key-1].ShowSameItem(key-1);
                     ShowAllItem();
                 }
+            }
+        }
+
+        public static void ClassifyItem()
+        {
+            Console.Clear();
+            Dictionary<ItemType,int> itemcount = new Dictionary<ItemType,int>();
+            
+            foreach(var item in itemlist)
+            {
+                if(!itemcount.ContainsKey(item.type))
+                {
+                    itemcount[item.type] = 1;
+                }
+                else
+                {
+                    itemcount[item.type]++;
+                }
+            }
+
+            foreach (var kvp in itemcount.Keys)
+            {
+                Console.WriteLine(kvp + ":" + itemcount[kvp]);
+            }
+            Console.WriteLine("Chosse to see list of item: ");
+            Console.WriteLine("1: Sowrd  2: Bow  3: Amor   4:Staff");
+            int select = int.Parse(Console.ReadLine());
+            if (select == 1)
+            {
+                ShowItemClassify(ItemType.Sword);
+            }
+            else if (select == 2)
+            {
+                ShowItemClassify(ItemType.Bow);
+            }
+            else if (select == 3)
+            {
+                ShowItemClassify(ItemType.Amor);
+            }
+            else if (select == 4)
+            {
+                ShowItemClassify(ItemType.Staff);
+            }
+            else
+            {
+                GameMenu();
+            }
+            Console.ReadKey();
+        }
+
+        public static void ShowItemClassify(ItemType typeclass)
+        {
+            Console.Clear();
+            foreach (var item in Itemclassify[typeclass]) 
+            {
+                Console.WriteLine("-------------");
+                Console.WriteLine("Name: " + item.itemname);
+                Console.WriteLine("rarity: " + item.rarity);
             }
         }
     }
